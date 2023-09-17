7312ac9627ccfa2c5e8d3e5eefd61072645ff1c6
BYREDDI RAMANA BABU <127107319+BYREDDI@users.noreply.github.com>
2023-09-17 18:04:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let arr=[];
+	var quo=num/2;
+	while(quo!=0)
+		{
+			let rem=num%2;
+			arr.unshift(rem);
+			quo=num/2;
+			num=quo;
+		}
+  return arr;
 }
 
 window.decimalToBinary = decimalToBinary;
